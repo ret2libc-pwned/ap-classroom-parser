5e5109865860abd71b80ff4119bc89d03968a415
ret2libc-pwned <binrays752@gmail.com>
2025-04-01 20:37:25 +0800

    fix error when the first option is  in json

--- get_sg.py
+++ get_sg.py
@@ -1,11 +1,5 @@
 import json
 
-with open("db.json", "r") as fin:
-	data = json.load(fin)
-
-all_questions = [item['questions'][0] for item in data['data']['apiActivity']['items']]
-all_tags = [tag_item for tag_item in data['data']['apiActivity']['tags'].values()]
-
 def get_question_statement(question):
 	return question['stimulus']
 
@@ -21,7 +15,9 @@ def stringify_options(options):
 	return result_html
 
 def get_answer_choice(question):
-	return ['A', 'B', 'C', 'D', 'E'][int(question['validation']['valid_response']['value'][0][1])]
+	table = ['A', 'B', 'C', 'D', 'E'] if start_from_zero else ['', 'A', 'B', 'C', 'D', 'E']
+	int(question['validation']['valid_response']['value'][0][1])
+	return table[int(question['validation']['valid_response']['value'][0][1])]
 
 def get_score(question):
 	return question['validation']['valid_response']['score']
@@ -37,31 +33,47 @@ def stringify_tag(tag):
 	result_html += "</list>\n"
 	return result_html
 
-question_no = 1
+def is_start_from_zero():
+	start_from_zero = False 
+	global all_questions
+	if int(all_questions[0]['options'][0]['value'][1]) == 0:
+		start_from_zero = True
+	return start_from_zero
 
-name = data['data']['apiActivity']['questionsApiActivity']['name']
+if __name__ == '__main__':
+	with open("db.json", "r") as fin:
+		data = json.load(fin)
 
-sg_html = f'''
-<title>Scoring Guide for {name} (AP Classroom Parser)</title>
-<h1>Scoring Guide for {name}</h1>
-<b>Important:</b> This document is parsed from students' client-side data package (from a bug), which <i>should not</i> meant to be got by students themselves. 
-<b>DO NOT SHARE THIS DOCUMENT!!</b>
-'''
-for question in all_questions:
-	sg_html += f'''<h2>Question {question_no} [{get_score(question)} pt (s)]</h2>
-	{get_question_statement(question)}
-	<h3>Choices</h3>
-	{stringify_options(get_options(question))}
-	<h3>Answer</h3>
-	{get_answer_choice(question)}
-	<h3>Tags</h3>
-	{stringify_tag(get_tag(question_no))}
-	<hr>
+	all_questions = [item['questions'][0] for item in data['data']['apiActivity']['items']]
+	all_tags = [tag_item for tag_item in data['data']['apiActivity']['tags'].values()]
+	start_from_zero = is_start_from_zero()
+
+	name = data['data']['apiActivity']['questionsApiActivity']['name']
+
+	sg_html = f'''
+	<title>Scoring Guide for {name} (AP Classroom Parser)</title>
+	<h1>Scoring Guide for {name}</h1>
+	<b>Important:</b> This document is parsed from students' client-side data package (from a bug), which <i>should not</i> meant to be got by students themselves. 
+	<b>DO NOT SHARE THIS DOCUMENT!!</b>
 	'''
-	question_no += 1
 
-with open("scoring_guide.html", "w") as fout:
-	fout.write(sg_html)
+	question_no = 1
 
-import os
-os.system("open scoring_guide.html")
+	for question in all_questions:
+		sg_html += f'''<h2>Question {question_no} [{get_score(question)} pt (s)]</h2>
+		{get_question_statement(question)}
+		<h3>Choices</h3>
+		{stringify_options(get_options(question))}
+		<h3>Answer</h3>
+		{get_answer_choice(question)}
+		<h3>Tags</h3>
+		{stringify_tag(get_tag(question_no))}
+		<hr>
+		'''
+		question_no += 1
+
+	with open("scoring_guide.html", "w") as fout:
+		fout.write(sg_html)
+
+	import os
+	os.system("open scoring_guide.html")
